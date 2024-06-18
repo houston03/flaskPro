0030b88195aec8dee584e2a44062cdf30c2ca334
houston03 <152857475+houston03@users.noreply.github.com>
2024-06-18 11:54:09 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+    data = [63, 70, 74, 76, 85, 89, 93, 99, 105, 111, 113, 117]
+    return render_template(
+        template_name_or_list='index.html',
+        data=data,
+        labels=labels,
+    )
+
+
+@app.route('/more')
+def more():
+    return render_template('more.html')
+
+
